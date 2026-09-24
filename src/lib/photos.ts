5c93088641023ef4071c0fc1577b18301de575db
556n0259 <556n.0259@gmail.com>
2026-09-24 20:53:16 +0900

    写真追加&データ編集

--- src/lib/photos.ts
+++ src/lib/photos.ts
@@ -13,6 +13,14 @@ export const photos: Photo[] = [
     image: "/photos/2_02.jpg",
   },
   {
+    grade: 2,
+    image: "/photos/2_03.jpg",
+  },
+  {
+    grade: 2,
+    image: "/photos/2_04.JPG",
+  },
+  {
     grade: 1,
     image: "/photos/1_01.JPG",
   },
@@ -23,5 +31,17 @@ export const photos: Photo[] = [
   {
     grade: 1,
     image: "/photos/1_03.jpg",
+  },
+  {
+    grade: 1,
+    image: "/photos/1_04.jpg",
+  },
+  {
+    grade: 1,
+    image: "/photos/1_05.JPG",
+  },
+  {
+    grade: 1,
+    image: "/photos/1_06.jpg",
   },
 ];
